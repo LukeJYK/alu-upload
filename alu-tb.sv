e06ecc0b9c376999e9cf56beb74cc5db8b3435a9
LukeJYK <809698350@qq.com>
2021-10-28 17:14:12 -0500

    success 32 add

--- alu-tb.sv
+++ alu-tb.sv
@@ -1,5 +1,12 @@
 -v200x -64bit -gui -access r
-./eecs361/lib/eecs361_gates.vhd
+eecs361/lib/xor_gate.vhd
+eecs361/lib/and_gate.vhd
+eecs361/lib/or_gate.vhd
+eecs361/lib/eecs361_gates.vhd
+full_adder.vhd 
 half_adder.vhd
-half_adder_t.vhd -top half_adder_t
+adder_8bit.vhd
+adder_32bit.vhd
+adder_32bit_t.vhd
+-top adder_32bit_t
 
